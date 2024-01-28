8dda8f629f550f7bade78006c72b42b5799051e0
Aleksey-Markov <tte4enuxa@gmail.com>
2024-01-28 19:24:48 +0700

    добавил фикстуру в тест

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -2,9 +2,14 @@ import pytest
 from pytest_proj.utils.dicts import get_val
 
 
-def test_get_val():
-    assert get_val({1: 'first', 2: 'second'}, 1) == 'first'
-    assert get_val({1: 'first', 2: 'second'}, 2) == 'second'
+@pytest.fixture()
+def dictionary():
+    return {1: 'first', 2: 'second'}
+
+
+def test_get_val(dictionary):
+    assert get_val(dictionary, 1) == 'first'
+    assert get_val(dictionary, 2) == 'second'
 
 
 def test_get_val_empty_dict():
@@ -12,6 +17,6 @@ def test_get_val_empty_dict():
     assert get_val({}, 2, 'python') == 'python'
 
 
-def test_get_val_wrong_key():
-    assert get_val({1: 'first', 2: 'second'}, 3) == 'git'
-    assert get_val({1: 'first', 2: 'second'}, 'git', 'python') == 'python'
+def test_get_val_wrong_key(dictionary):
+    assert get_val(dictionary, 3) == 'git'
+    assert get_val(dictionary, 'git', 'python') == 'python'
